9d86699fc6a2a37685dc1ecacbeffa4b90c507b4
Kathy <kathy.wazowski@gmail.com>
2025-01-13 17:30:56 +0800

    Add: add experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -21,6 +22,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
 
