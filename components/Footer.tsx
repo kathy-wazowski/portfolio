b2758c3dee73b8d0529ed9bdc503705a54c172af
Kathy <kathy.wazowski@gmail.com>
2025-01-15 16:29:27 +0800

    add: add socialMedia to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import MagicButton from '../components/ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/Data'
+
 
 const Footer = () => {
     return (
@@ -11,8 +15,32 @@ const Footer = () => {
                 <h1 className="heading lg:max-w-[45vw]">
                     Ready to take <span className="text-purple">your</span> digital presence to next level?
                 </h1>
+                <p className="text-white-200 md:mt-10 my-5 text-center">
+                    Reach out to me today and let's discuss how I can help you achieve your goals.
+                </p>
+                <a href="mailto:kathy.wazowski@gmail.com">
+                    <MagicButton title="Let's get in touch" icon={<FaLocationArrow />} position="right" />
+                </a>
+            </div>
+
+            <div className="flex mt-16 md:flew-row flex-col justify-between items-center">
+                <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2025 Kathy</p>
+                <div className="flex items-center md:gap-3 gap-6">
+                    {socialMedia.map(profile => (
+                        <div key={profile.id} className="w-10 h-10 cursor-pointer flex items-center justify-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border-black-300">
+                            <img src={profile.img} alt={profile.id.toString()} width={20} height={20} />
+                        </div>
+                    ))}
+
+
+                </div>
+
             </div>
         </footer>
+
+
+
+
 
 
     )
